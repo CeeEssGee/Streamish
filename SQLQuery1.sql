fb007d5b27f69ad08482ba1105068f85c6aca8ce
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-07-07 14:56:36 -0400

    initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,24 @@
+﻿SELECT * FROM Video;
+SELECT * FROM Favorite;
+SELECT * FROM Comment;
+SELECT * FROM UserProfile;
+
+--Get all videos
+SELECT v.Id, v.Title, v.Description, v.Url, v.DateCreated, v.UserProfileId,
+                      up.Name, up.Email, up.DateCreated AS UserProfileDateCreated,
+                      up.ImageUrl AS UserProfileImageUrl
+                 FROM Video v 
+                      JOIN UserProfile up ON v.UserProfileId = up.Id
+             ORDER BY DateCreated
+
+
+--Get all videos with comments
+SELECT v.Id AS VideoId, v.Title, v.Description, v.Url, 
+                       v.DateCreated AS VideoDateCreated, v.UserProfileId As VideoUserProfileId,
+                     up.Name, up.Email, up.DateCreated AS UserProfileDateCreated,
+                       up.ImageUrl AS UserProfileImageUrl,
+                       c.Id AS CommentId, c.Message, c.UserProfileId AS CommentUserProfileId
+                  FROM Video v 
+                       JOIN UserProfile up ON v.UserProfileId = up.Id
+                       LEFT JOIN Comment c on c.VideoId = v.id
+             ORDER BY  v.DateCreated
